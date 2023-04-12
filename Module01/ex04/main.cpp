eff1d1b064512e1e5269a9761e622186260a38b3
Cristian <moralesrojascr@gmail.com>
2023-04-12 11:33:51 +0200

    finish ex04 Module01

--- Module01/ex04/main.cpp
+++ Module01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cmorales <moralesrojascr@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 12:21:27 by cmorales          #+#    #+#             */
-/*   Updated: 2023/04/11 20:51:06 by cmorales         ###   ########.fr       */
+/*   Updated: 2023/04/12 01:10:17 by cmorales         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,9 @@ std::string get_content_file(std::string filename)
 {
 	std::ifstream file;
 	std::string aux;
-
-	file.open(filename, std::ios::in);
+	const char* lynux_filename = filename.c_str();
+	
+	file.open(lynux_filename, std::ios::in);
 	if(!file.is_open())
 		std::cout << "Error: File cannot be opened"<< std::endl;
 	else
@@ -36,24 +37,24 @@ void replace_string(std::string filename, std::string s1, std::string s2)
 	std::string aux;
 	std::ofstream out_file;
 	size_t	n_find;
+
 	(void)s2;
 	file = get_content_file(filename);
-	out_file.open(filename + ".replace");
+	filename = filename + ".replace";
+	const char* lynux_filename = filename.c_str();
+	out_file.open(lynux_filename);
 	if(!out_file.is_open())
 		std::cout << "Error: File cannot be opened"<< std::endl;
 	else
 	{
-		int last = 0;
-		n_find = file.find(s1, last);
-		while(n_find != std::string::npos)
+		//int last = 0;
+		while((n_find = file.find(s1)) != std::string::npos)
 		{
 			aux = file.substr(n_find + s1.size());
 			file.erase(n_find);
-			//file += s2 + aux;
+			file += s2 + aux;
 			//last = n_find + s2.size();
-			break;
 		}
-		std::cout<<"Es: "<<aux<<std::endl;
 		out_file << file;
 		out_file.close();
 	}
@@ -67,6 +68,8 @@ int main(int ac, char **av)
 		std::string aux = av[1];
 		std::string s1 = av[2];
 		std::string s2 = av[3];
+		
+
 		replace_string(filename, s1, s2);
 	}
 	else
